686703264604e47fc2a5a4240f6554bcaab21d07
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-01-19 12:30:49 -0800

    Class work code through

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -5,8 +5,16 @@
 
   repos.requestRepos = function(callback) {
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    //fetch repos with ajax
+    //write a success function to do something
+    var qs - '?per_page=100&sort=updated';
+    $.get('/github/users/nadiabahrami/repos' +qs)
+    .done(function(data, message, xhr){
+      repos.all = data;
+    })
+    .done(callback);
 
-  };
+    };
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
